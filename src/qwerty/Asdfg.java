e865f44a42720a19260871f5bb8108ff0837c9b5
ozangulenc <ozangulenc90@gmail.com>
2022-04-08 16:30:26 +0300

    2. commitimiz

--- src/qwerty/Asdfg.java
+++ src/qwerty/Asdfg.java
@@ -3,5 +3,8 @@ package qwerty;
 public class Asdfg {
     public static void main(String[] args) {
         System.out.println(" Ozan gulenc");
+
+        System.out.println("Selamınaleyküm");
+
     }
 }
